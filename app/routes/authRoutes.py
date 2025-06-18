2154d48c76191ff3419e6ae791f1e1e6c29325f4
Mitchellesweetie <mitchellengoli@gmail.com>
2025-06-18 16:10:17 +0300

    logout

--- app/routes/authRoutes.py
+++ app/routes/authRoutes.py
@@ -2,6 +2,10 @@ from fastapi import APIRouter
 from models.stakeholders import LoginPayload, StakeholderSignup
 from controllers.auth.signupController import signup_user
 from controllers.auth.loginController import login_user
+from controllers.auth.sessionController import logout_user
+from controllers.auth.sessionController import refresh_token
+
+
 
 router = APIRouter()
 
@@ -12,3 +16,8 @@ async def signup_route(payload:StakeholderSignup):
 @router.post("/signin", tags=["Auth"])
 async def signin_route(payload:LoginPayload):
     return await login_user(payload)
+
+@router.get("/logout",tags=["Auth"])
+async def signout():
+    return await logout_user()
+
